ce9b1b96271a83594b5573befc536474d9814f5d
SirAlyon <alessiosada04@gmail.com>
2022-03-25 17:53:18 +0100

    Final Commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,7 @@ va applicato uno sconto del 40% per gli over 65. */
 let userFullName = document.getElementById("full_name").value
 console.log(userFullName);
 
-let trip_km = document.getElementById("trip_km").value
+let trip_km = Number(document.getElementById("trip_km").value)
 console.log(trip_km);
 
 let user_age = document.getElementById("age").value
@@ -18,6 +18,11 @@ console.log(user_age);
 let trip_cost = trip_km * 0.21
 
 
+//blocchiamo l'utente se scrive testo nei km da percorrere
+if (isNaN(trip_km)) {
+    alert("Per favore inserisci un valore numerico per indicare la distanza che vuoi percorrere in treno!")
+}
+
 //calcoliamo gli sconti in base all'età dell'utente
 if (user_age === 'minorenne') {
     trip_cost = (trip_km * 0.21) * 0.80
@@ -25,22 +30,28 @@ if (user_age === 'minorenne') {
     trip_cost = (trip_km * 0.21) * 0.60
 } 
 
-
-console.log(trip_cost.toFixed(2) + " €")
-
-//stampiamo il risultato all'utente
-
-//bonus - avvisiamo l'utente se ha ricevuto uno sconto
-
-
-
-
+//creazione biglietto
 document.getElementById("generate").addEventListener("click", function(){
-    document.getElementById("p-name").innerHTML = userFullName
-    document.getElementById("discount").innerHTML = `SCONTO ${user_age}`
+    document.getElementById("p-name").innerHTML = userFullName.toUpperCase()
+    document.getElementById("discount").innerHTML = `SCONTO ${user_age.toUpperCase()}`
     document.getElementById("carrozza").innerHTML = Math.floor(Math.random() * 10) + 1
     document.getElementById("cp").innerHTML = Math.floor(Math.random() * 10000) + 1
     document.getElementById("trip_cost").innerHTML = `${trip_cost.toFixed(2)} €`
-
-
+    if (user_age ==='maggiorenne') {
+        document.getElementById("discount").innerHTML = `SCONTO NON DISPONIBILE`
+    }
+    if(isNaN(trip_cost)){
+        document.getElementById("trip_cost").innerHTML = `IMPOSSIBILE CALCOLARE LA TARIFFA, RICONTROLLA I DATI INSERITI`
+    }
 });
+
+document.getElementById("delete").addEventListener("click", function(){
+    document.getElementById("p-name").innerHTML = ''
+    document.getElementById("discount").innerHTML = ''
+    document.getElementById("carrozza").innerHTML = ''
+    document.getElementById("cp").innerHTML = ''
+    document.getElementById("trip_cost").innerHTML = ''
+    document.getElementById("full_name").value = ''
+    document.getElementById("trip_km").value = ''
+    document.getElementById("age").value = ''
+}); 
